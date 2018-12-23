ce777e7e1b1ee57708a66b93087502ab2445e374
gali <gbmatrix@gmail.com>
2018-12-23 16:07:25 +0200

    added CART

--- public/app.js
+++ public/app.js
@@ -1,16 +1,12 @@
-function something() {
+unction add_to_cart(id) {
+   
+   var count = window.localStorage.getItem(id);
+   count = count * 1 + 1; 
+   window.localStorage.setItem(id, count);
+   	
+
+	alert("Added to Cart " +  count);
  
-   var x = window.localStorage.getItem('aaa');
-   x = x * 1 + 1; 
-   window.localStorage.setItem("aaa", x);
-  
-   alert(x);
-
-}
 
 
-funcion add_to_cart() {
-
-	alert("Added to Cart!");
-
-}
+}  
